ece93fd92d75a3cc0bd82e786cf777b8871c55f9
Corey Oordt <coordt@washingtontimes.com>
2009-07-07 07:31:34 -0400

    Added in support materials

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='django-categories',
+      version='0.1',
+      description='A way to handle one or more hierarchical category trees in django.',
+      long_description='This app attempts to provide a generic category system that multiple apps could use. It uses MPTT for the tree storage and provides a custom admin for better visualization (copied and modified from feinCMS).',
+      author='Corey Oordt',
+      author_email='coordt@washingtontimes.com',
+      url='http://opensource.washingtontimes.com/projects/django-categories/',
+      packages=['django-categories'],
+      classifiers=['Development Status :: 4 - Beta',
+          'Framework :: Django',
+          'License :: OSI Approved :: Apache License',
+          ],
+      )
+
