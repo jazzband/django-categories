40f5471abd512ee4db7eddce780b4b49bcc7cd68
Corey Oordt <coordt@washingtontimes.com>
2009-08-25 20:52:46 -0400

    Split the editor into a separate app

--- categories/bin/refresh_paths.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from ellington.categories.models import Category
-
-roots = Category.objects.filter(parent__isnull=True)
-
-for root in roots:
-    root.refresh_paths(force=True)
-
