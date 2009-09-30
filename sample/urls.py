2e9cba224cfe46a72c3ec078a4d559679abd6e52
Corey Oordt <coordt@washingtontimes.com>
2009-09-30 10:53:09 -0400

    Added some sample config to see it work

---
+++ sample/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+
+import os
+
+ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^sample/', include('sample.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+
+    (r'^static/categories/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': ROOT_PATH + '/categories/media/categories/'}),
+
+    (r'^static/editor/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': ROOT_PATH + '/editor/media/editor/',
+         'show_indexes':True}),
+)
