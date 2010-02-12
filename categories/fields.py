69ef59d178778f839b4a470f5cdba2506376dd8c
Corey Oordt <coordt@washingtontimes.com>
2010-02-12 12:09:58 -0500

    Removed some tabs...

--- categories/fields.py
+++ categories/fields.py
@@ -4,8 +4,8 @@ class CategoryM2MField(ManyToManyField):
     def __init__(self, **kwargs):
         from categories.models import Category
         super(CategoryM2MField, self).__init__(to=Category, **kwargs)
-    
-    
+
+
 class CategoryFKField(ForeignKey):
     def __init__(self, **kwargs):
         from categories.models import Category
