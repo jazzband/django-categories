d1bf06b39f54fab1be466d45460c9f3fd15ee7d8
Corey Oordt <coordt@washingtontimes.com>
2010-12-16 11:47:08 -0500

    Don't need to set that EDITOR_MEDIA_PATH any more

--- example/settings.py
+++ example/settings.py
@@ -72,6 +72,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates')),
 )
 
 INSTALLED_APPS = (
@@ -86,6 +87,6 @@ INSTALLED_APPS = (
     'mptt',
     'simpletext',
 )
-EDITOR_MEDIA_PATH = '/static/editor/'
+#EDITOR_MEDIA_PATH = '/static/editor/'
 CATEGORIES_ALLOW_SLUG_CHANGE = True
 CATEGORIES_RELATION_MODELS = ['simpletext.simpletext','flatpages.flatpage']
