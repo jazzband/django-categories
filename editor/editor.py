6693d8dad3089881d2f8fddd1893461bf291bbfd
Corey Oordt <coordt@washingtontimes.com>
2010-12-16 11:38:55 -0500

    Removed unused code files

--- editor/editor.py
+++
@@ -1,4 +0,0 @@
-#from feincms.admin.item_editor import ItemEditor, ItemEditorForm
-from tree_editor import TreeEditor, ajax_editable_boolean, \
-    ajax_editable_boolean_cell, django_boolean_icon
-from splitpane_editor import SplitPaneEditor
