781f9d02d50fee13c10aa3a959ba15f364105f52
Corey Oordt <coordt@washingtontimes.com>
2010-12-16 13:28:23 -0500

    Added additional fields to the display_list

--- categories/admin.py
+++ categories/admin.py
@@ -76,7 +76,7 @@ class CategoryAdminForm(forms.ModelForm):
 
 class CategoryAdmin(TreeEditor, admin.ModelAdmin):
     form = CategoryAdminForm
-    list_display = ('name',)
+    list_display = ('name','order','alternate_title',)
     search_fields = (('name',))
     prepopulated_fields = {'slug': ('name',)}
     fieldsets = (
