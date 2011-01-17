8f8bc6eb96f50c24ccffa3aea9e9cdc3e508cfa3
Erik Simmler <erik@simmlers.com>
2011-01-17 11:09:16 -0500

    removed duplicate slash from EDITOR_MEDIA_PATH setting

--- editor/settings.py
+++ editor/settings.py
@@ -4,4 +4,4 @@ import django
 DJANGO10_COMPAT = django.VERSION[0] < 1 or (django.VERSION[0] == 1 and django.VERSION[1] < 1)
 
 STATIC_URL = getattr(settings, 'STATIC_URL', settings.MEDIA_URL)
-MEDIA_PATH = getattr(settings, 'EDITOR_MEDIA_PATH', '%s/editor/' % STATIC_URL)
+MEDIA_PATH = getattr(settings, 'EDITOR_MEDIA_PATH', '%seditor/' % STATIC_URL)
