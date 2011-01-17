44508e060616025d145637deb0b1bbc652a919fe
Erik Simmler <erik@simmlers.com>
2011-01-17 10:00:30 -0500

    ModelAdmin re-register now skips modeladmins without fieldsets already defined
    Was causing a "TypeError at /current/url/: unsupported operand type(s) for +: 'NoneType' and 'tuple'"

--- categories/admin.py
+++ categories/admin.py
@@ -102,7 +102,7 @@ class CategoryAdmin(TreeEditor, admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 
 for model,modeladmin in admin.site._registry.items():
-    if model in registry.values():
+    if model in registry.values() and modeladmin.fieldsets:
         admin.site.unregister(model)
         admin.site.register(model, type('newadmin', (modeladmin.__class__,), {
             'fieldsets': getattr(modeladmin, 'fieldsets', ()) + (('Categories',{
