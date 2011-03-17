29ab0943c5b67240d0ed0271b4efd2d947ad497b
Martin Ogden <martin@cahoona.co.uk>
2011-03-17 18:20:53 +0800

    Remove 'to' from kwargs in CategoryM2MField and CategoryFKField. 'to' is already specified, and causes errors when running unit tests

--- categories/fields.py
+++ categories/fields.py
@@ -1,12 +1,18 @@
 from django.db.models import ForeignKey, ManyToManyField
 
+from categories.models import Category
+
+
+
 class CategoryM2MField(ManyToManyField):
     def __init__(self, **kwargs):
-        from categories.models import Category
+        if 'to' in kwargs:
+            kwargs.pop('to')
         super(CategoryM2MField, self).__init__(to=Category, **kwargs)
 
 
 class CategoryFKField(ForeignKey):
     def __init__(self, **kwargs):
-        from categories.models import Category
+        if 'to' in kwargs:
+            kwargs.pop('to')
         super(CategoryFKField, self).__init__(to=Category, **kwargs)
