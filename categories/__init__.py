6f26261c50d0b60ba8833d7186156d268f989fd7
Corey Oordt <coordt@washingtontimes.com>
2011-05-18 07:26:54 -0400

    Version bump to 0.6

--- categories/__init__.py
+++ categories/__init__.py
@@ -2,8 +2,8 @@ __version_info__ = {
     'major': 0,
     'minor': 6,
     'micro': 0,
-    'releaselevel': 'beta',
-    'serial': 5
+    'releaselevel': 'final',
+    'serial': 1
 }
 
 def get_version():
