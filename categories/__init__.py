309accf3e0ca8affa346ff0d1bec193fb6f99780
Corey Oordt <coordt@washingtontimes.com>
2011-08-19 10:05:25 -0400

    Updated the get_version function to be PEP 386 compliant and version bump to 0.7.2b1

--- categories/__init__.py
+++ categories/__init__.py
@@ -1,18 +1,18 @@
 __version_info__ = {
     'major': 0,
     'minor': 7,
-    'micro': 1,
-    'releaselevel': 'final',
+    'micro': 2,
+    'releaselevel': 'beta',
     'serial': 1
 }
 
-def get_version():
+def get_version(short=False):
+    assert __version_info__['releaselevel'] in ('alpha', 'beta', 'final')
     vers = ["%(major)i.%(minor)i" % __version_info__, ]
-
     if __version_info__['micro']:
         vers.append(".%(micro)i" % __version_info__)
-    if __version_info__['releaselevel'] != 'final':
-        vers.append('%(releaselevel)s%(serial)i' % __version_info__)
+    if __version_info__['releaselevel'] != 'final' and not short:
+        vers.append('%s%i' % (__version_info__['releaselevel'][0], __version_info__['serial']))
     return ''.join(vers)
 
 __version__ = get_version()
