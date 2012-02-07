65ef584cf475c8c83c3381a410a78f1a971e247b
Corey Oordt <coreyoordt@gmail.com>
2012-02-07 13:07:51 -0500

    Version bump to 1.0b1

--- categories/__init__.py
+++ categories/__init__.py
@@ -1,8 +1,8 @@
 __version_info__ = {
-    'major': 0,
-    'minor': 8,
-    'micro': 9,
-    'releaselevel': 'final',
+    'major': 1,
+    'minor': 0,
+    'micro': 0,
+    'releaselevel': 'beta',
     'serial': 1
 }
 
