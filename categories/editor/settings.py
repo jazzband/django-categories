8a621e9d145be2b187ccd09bd6a96e421cc5ea4e
Brent O'Connor <epicserve@gmail.com>
2012-08-27 16:56:12 -0700

    Simplified the assignment of the IS_GRAPPELLI_INSTALLED variable

--- categories/editor/settings.py
+++ categories/editor/settings.py
@@ -10,4 +10,4 @@ MEDIA_PATH = getattr(settings, 'EDITOR_MEDIA_PATH', '%seditor/' % STATIC_URL)
 
 TREE_INITIAL_STATE = getattr(settings, 'EDITOR_TREE_INITIAL_STATE', 'collapsed')
 
-IS_GRAPPELLI_INSTALLED = True if 'grappelli' in settings.INSTALLED_APPS else False
+IS_GRAPPELLI_INSTALLED = 'grappelli' in settings.INSTALLED_APPS
