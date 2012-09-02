89c733330617c9964ac8e25469274a4dbcb3eda2
winniehell <git@winniehell.de>
2012-09-02 16:03:58 +0200

    i18n

--- categories/settings.py
+++ categories/settings.py
@@ -2,6 +2,7 @@ import warnings
 
 from django.conf import settings
 from django.db.models import Q
+from django.utils.translation import ugettext_lazy as _
 
 DEFAULT_SETTINGS = {
     'ALLOW_SLUG_CHANGE': False,
@@ -29,26 +30,29 @@ if DEFAULT_SETTINGS['SLUG_TRANSLITERATOR']:
         DEFAULT_SETTINGS['SLUG_TRANSLITERATOR'] = getattr(module, bits[-1])
     else:
         from django.core.exceptions import ImproperlyConfigured
-        raise ImproperlyConfigured("SLUG_TRANSLITERATOR must be a callable or a string.")
+        raise ImproperlyConfigured(_('%(transliterator) must be a callable or a string.') %
+                                   {'transliterator' : 'SLUG_TRANSLITERATOR'})
 else:
     DEFAULT_SETTINGS['SLUG_TRANSLITERATOR'] = lambda x: x
 
-ERR_MSG = "settings.%s is deprecated; use settings.CATEGORIES_SETTINGS instead."
+def warn_deprecated(deprecated_setting, replacement):
+    warnings.warn(_('%(deprecated_setting) is deprecated; use %(replacement)s instead.') %
+                  {'deprecated_setting' : deprecated_setting, 'replacement' : replacement}, DeprecationWarning)
 
 if hasattr(settings, 'CATEGORIES_ALLOW_SLUG_CHANGE'):
-    warnings.warn(ERR_MSG % 'CATEGORIES_ALLOW_SLUG_CHANGE', DeprecationWarning)
+    warn_deprecated('settings.CATEGORIES_ALLOW_SLUG_CHANGE', 'settings.CATEGORIES_SETTINGS')
     DEFAULT_SETTINGS["ALLOW_SLUG_CHANGE"] = getattr(settings, 'CATEGORIES_ALLOW_SLUG_CHANGE')
 
 if hasattr(settings, 'CATEGORIES_CACHE_VIEW_LENGTH'):
-    warnings.warn(ERR_MSG % "CATEGORIES_CACHE_VIEW_LENGTH", DeprecationWarning)
+    warn_deprecated('settings.CATEGORIES_CACHE_VIEW_LENGTH', 'settings.CATEGORIES_SETTINGS')
     DEFAULT_SETTINGS["CACHE_VIEW_LENGTH"] = getattr(settings, 'CATEGORIES_CACHE_VIEW_LENGTH')
 
 if hasattr(settings, 'CATEGORIES_THUMBNAIL_UPLOAD_PATH'):
-    warnings.warn(ERR_MSG % "CATEGORIES_THUMBNAIL_UPLOAD_PATH", DeprecationWarning)
+    warn_deprecated('settings.CATEGORIES_THUMBNAIL_UPLOAD_PATH', 'settings.CATEGORIES_SETTINGS')
     DEFAULT_SETTINGS["THUMBNAIL_UPLOAD_PATH"] = getattr(settings, 'CATEGORIES_THUMBNAIL_UPLOAD_PATH')
 
 if hasattr(settings, 'CATEGORIES_RELATION_MODELS'):
-    warnings.warn(ERR_MSG % "CATEGORIES_RELATION_MODELS", DeprecationWarning)
+    warn_deprecated('settings.CATEGORIES_RELATION_MODELS', 'settings.CATEGORIES_SETTINGS')
     DEFAULT_SETTINGS["RELATION_MODELS"] = getattr(settings, 'CATEGORIES_RELATION_MODELS')
 
 # Add all the keys/values to the module's namespace
