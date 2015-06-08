8b8c22a359187eb7f9125ea328ad5cddcef57676
Brent O'Connor <epicserve@gmail.com>
2015-06-08 09:19:40 -0500

    Fixed not being able to add new categories in the Django admin.
    An exception was raised when saving new categories because .get_descendants() was being called on new instances that haven't been saved to the database yet.

--- categories/base.py
+++ categories/base.py
@@ -108,13 +108,15 @@ class CategoryBaseAdminForm(forms.ModelForm):
         # Validate Category Parent
         # Make sure the category doesn't set itself or any of its children as
         # its parent.
-        decendant_ids = self.instance.get_descendants().values_list('id', flat=True)
+        descendant_ids = []
+        if self.instance.id is not None:
+            descendant_ids = self.instance.get_descendants().values_list('id', flat=True)
         if self.cleaned_data.get('parent', None) is None or self.instance.id is None:
             return self.cleaned_data
         elif self.cleaned_data['parent'].id == self.instance.id:
             raise forms.ValidationError(_("You can't set the parent of the "
                                           "item to itself."))
-        elif self.cleaned_data['parent'].id in decendant_ids:
+        elif self.cleaned_data['parent'].id in descendant_ids:
             raise forms.ValidationError(_("You can't set the parent of the "
                                           "item to a descendant."))
         return self.cleaned_data
