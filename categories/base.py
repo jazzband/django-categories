736bd2372fe8c74607fd8a4f05af57f46fce759c
Chris Barna <chris@unbrain.net>
2015-08-13 16:55:15 -0400

    Fix where you couldn't access things on unsaved instances. Bump to v1.2post-atl-1.4

--- categories/base.py
+++ categories/base.py
@@ -107,15 +107,17 @@ class CategoryBaseAdminForm(forms.ModelForm):
         # Validate Category Parent
         # Make sure the category doesn't set itself or any of its children as
         # its parent.
-        decendant_ids = self.instance.get_descendants().values_list('id', flat=True)
         if self.cleaned_data.get('parent', None) is None or self.instance.id is None:
             return self.cleaned_data
         elif self.cleaned_data['parent'].id == self.instance.id:
             raise forms.ValidationError(_("You can't set the parent of the "
                                           "item to itself."))
-        elif self.cleaned_data['parent'].id in decendant_ids:
-            raise forms.ValidationError(_("You can't set the parent of the "
-                                          "item to a descendant."))
+
+        if self.instance.pk:
+            decendant_ids = self.instance.get_descendants().values_list('id', flat=True)
+            if self.cleaned_data['parent'].id in decendant_ids:
+                raise forms.ValidationError(_("You can't set the parent of the "
+                                              "item to a descendant."))
         return self.cleaned_data
 
 
