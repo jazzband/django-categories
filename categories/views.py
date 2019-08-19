31adab12002a7c6e45ee44e2d5edc3cb44e06c86
Luke Gessler <ldg3fa@virginia.edu>
2019-08-19 01:30:05 -0400

    add import for redirect

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.http import HttpResponse, Http404
 from django.template.loader import select_template
 from django.utils.translation import ugettext_lazy as _
