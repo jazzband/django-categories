515816465a0451cb796316020c697ccdcbe05ccf
Petr Dlouhý <petr.dlouhy@email.cz>
2020-03-13 20:02:38 +0100

    fix ValueError in tests

--- categories/fields.py
+++ categories/fields.py
@@ -6,19 +6,15 @@ class CategoryM2MField(ManyToManyField):
     """A many to many field to a Category model."""
 
     def __init__(self, **kwargs):
-        from .models import Category
-
         if "to" in kwargs:
             kwargs.pop("to")
-        super(CategoryM2MField, self).__init__(to=Category, **kwargs)
+        super(CategoryM2MField, self).__init__(to="categories.Category", **kwargs)
 
 
 class CategoryFKField(ForeignKey):
     """A foreign key to the Category model."""
 
     def __init__(self, **kwargs):
-        from .models import Category
-
         if "to" in kwargs:
             kwargs.pop("to")
-        super(CategoryFKField, self).__init__(to=Category, **kwargs)
+        super(CategoryFKField, self).__init__(to="categories.Category", **kwargs)
