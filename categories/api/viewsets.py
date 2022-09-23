33fd44ea8e81a900aabf60a88b01da28315890c5
Petr Dlouhý <petr.dlouhy@email.cz>
2022-09-23 18:44:43 +0200

    API: add cumulative counts, add possibility to set extra_count_filters, cache result

--- categories/api/viewsets.py
+++ categories/api/viewsets.py
@@ -1,6 +1,7 @@
-from django.db.models import Count
-
 from rest_framework import mixins, serializers, viewsets
+from rest_framework import permissions
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 from ..models import Category
 
@@ -40,6 +41,13 @@ for field in countable_fields:
     setattr(CategorySerializer, f'get_{field.name}_count', field_count)
     CategorySerializer.Meta.fields += [f'{field.name}_count']
 
+    CategorySerializer._declared_fields[f'{field.name}_count_cumulative'] = serializers.SerializerMethodField()
+
+    def field_count_cumulative(self, obj, field=field):
+        return getattr(obj, f'{field.name}_count_cumulative', "-")
+    setattr(CategorySerializer, f'get_{field.name}_count_cumulative', field_count_cumulative)
+    CategorySerializer.Meta.fields += [f'{field.name}_count_cumulative']
+
 
 CategorySerializer._declared_fields['children'] = CategorySerializer(
     many=True,
@@ -51,18 +59,46 @@ class CategoryList(list):  # To overcome problem with filters that require model
     model = Category
 
 
+def get_category_queryset(queryset, extra_filters=None, exclude_blank=False):
+    if not extra_filters:
+        extra_filters = {}
+
+    for field in countable_fields:
+        queryset = Category.tree.add_related_count(
+            queryset, field.related_model, field.remote_field.name, f"{field.name}_count", extra_filters=extra_filters
+        )
+        queryset = Category.tree.add_related_count(
+            queryset,
+            field.related_model,
+            field.remote_field.name,
+            f"{field.name}_count_cumulative",
+            extra_filters=extra_filters,
+            cumulative=True,
+        )
+
+    if exclude_blank:
+        queryset = queryset.filter(asset_count_cumulative__gt=0)
+    return queryset
+
+
 class CategoryViewSet(
         mixins.ListModelMixin,
         viewsets.GenericViewSet):
     queryset = Category.tree.all()
     serializer_class = CategorySerializer
+    extra_count_filters = {}
 
     def get_queryset(self, queryset=None):
         if not queryset:
             queryset = self.queryset
 
-        for field in countable_fields:
-            queryset = queryset.annotate(**{field.name + '_count': Count(field.name)})
+        queryset = get_category_queryset(queryset, extra_filters=self.extra_count_filters)
 
         queryset = CategoryList(queryset.get_cached_trees())
         return queryset
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    @method_decorator(cache_page(60 * 60))
+    def list(self, *args, **kwargs):
+        return super().list(*args, **kwargs)
